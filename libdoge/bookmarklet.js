fc7987905047cf28f418cc99e043859321851b02
Lauri-Henrik Jalonen <ljalonen@cs.helsinki.fi>
2013-11-12 09:15:09 +0200

    such fix

--- libdoge/bookmarklet.js
+++ libdoge/bookmarklet.js
@@ -1,7 +1,7 @@
 if (typeof LIBDOGE == 'undefined') {
   var libdoge = document.createElement("script");
   libdoge.setAttribute("src",
-    "https://github.com/ljalonen/libdoge/libdoge/libdoge-min.js");
+    "https://raw.github.com/ljalonen/libdoge/master/libdoge/libdoge-min.js");
   libdoge.onload = function() {
     LIBDOGE.controller.buyDoge();
   }
